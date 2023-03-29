ffa48b69d5a5b4624bd7ff438bb9902d6b1c134e
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2023-03-28 20:27:41 -0400

    Updated docs

---
+++ docs/implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sttp":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"sttp/struct.Ticks.html\" title=\"struct sttp::Ticks\">Ticks</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"sttp":["",[["transport",[],["signal_index_cache.rs"]]],["config.rs","data.rs","lib.rs","settings.rs","transport.rs"]]\
+"sttp":["",[["transport",[],["signal_index_cache.rs"]]],["config.rs","data.rs","lib.rs","settings.rs","ticks.rs","transport.rs"]]\
 }');
 createSourceSidebar();
